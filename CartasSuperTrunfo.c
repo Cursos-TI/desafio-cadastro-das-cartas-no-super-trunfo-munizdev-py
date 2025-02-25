b3bcae56a5992c1b9af9428af85c9545a4c00dc0
munizdev-py <munizadscode@gmail.com>
2025-02-25 14:57:06 +0000

    Falta terminar o final do cod

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,11 +8,59 @@
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
+    char estado;
+    int codigo;
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosturisticos;
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+
+    printf("Cadastro de Cartas");
+                 printf("\n");
+
+      printf("Carta 01");
+    printf("Digite a letra do Estado (A - H): ");
+        scanf("%s", &estado);
+    printf("Digite o código da Carta (01 - 04): ");
+        scanf("%d", &codigo);
+    printf("Digite o nome da Cidade: ");
+        scanf("%s", &nome);
+    printf("Digite o número de populções: ");
+        scanf("%d", &populacao);
+    printf("Digite a Área (em km²): ");
+        scanf("%f", &area);
+    printf("Digite o PIB: ");
+        scanf("%f", &pib);
+    printf("Digite o número de pontos Turísticos: ");
+        scanf("%d", &pontosturisticos);
+
+                 printf("\n");
+    
+      printf("Carta 02");
+    printf("Digite a letra do Estado (A - H): ");
+        scanf("%s", &estado);
+    printf("Digite o código da Carta (01 - 04): ");
+        scanf("%d", &codigo);
+    printf("Digite o nome da Cidade: ");
+        scanf("%s", &nome);
+    printf("Digite o número de populções: ");
+        scanf("%d", &populacao);
+    printf("Digite a Área (em km²): ");
+        scanf("%f", &area);
+    printf("Digite o PIB: ");
+        scanf("%f", &pib);
+    printf("Digite o número de pontos Turísticos: ");
+        scanf("%d", &pontosturisticos);
+
+
+
+
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
